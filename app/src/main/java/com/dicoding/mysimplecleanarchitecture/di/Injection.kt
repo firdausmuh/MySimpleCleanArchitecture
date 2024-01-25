78d482518de6eb82085a65c2cc7b5fe94fd6906c
firdausmuh <muhfrds345@gmail.com>
2024-01-25 16:12:37 +0700

    Initial commit cleanArchitecture

---
+++ app/src/main/java/com/dicoding/mysimplecleanarchitecture/di/Injection.kt
@@ -0,0 +1,26 @@
+package com.dicoding.mysimplecleanarchitecture.di
+
+import com.dicoding.mysimplecleanarchitecture.data.IMessageDataSource
+import com.dicoding.mysimplecleanarchitecture.data.MessageDataSource
+import com.dicoding.mysimplecleanarchitecture.data.MessageRepository
+import com.dicoding.mysimplecleanarchitecture.domain.IMessageRepository
+import com.dicoding.mysimplecleanarchitecture.domain.MessageInteractor
+import com.dicoding.mysimplecleanarchitecture.domain.MessageUseCase
+
+// class Injection di buat agar untuk melakukan provide usecase di ViewModelFactory
+
+object Injection {
+    fun provideUseCase(): MessageUseCase {
+        val messageRepository = provideRepository()
+        return MessageInteractor(messageRepository)
+    }
+
+    private fun provideRepository(): IMessageRepository {
+        val messageDataSource = provideDataSource()
+        return MessageRepository(messageDataSource)
+    }
+
+    private fun provideDataSource(): IMessageDataSource {
+        return MessageDataSource()
+    }
+}
